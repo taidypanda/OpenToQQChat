a45792eb05a7d38fee73246602964769a3f66b10
苹果 <pingguo@pingguodemacbook-pro.local>
2016-05-25 11:58:06 +0800

    update file

--- OpenToQQChat/ViewController.m
+++ OpenToQQChat/ViewController.m
@@ -18,6 +18,16 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)goPikaChu:(id)sender {
+    // 空白的webview
+    UIWebView * webView = [[UIWebView alloc] initWithFrame:CGRectZero];
+    //貌似现在这个不能用了
+    //    NSString * str = [NSString stringWithFormat:@"http://wpa.qq.com/msgrd?v=3&uin=%@&site=qq&menu=yes", @"742308143"];
+    //记得plist文件里面要添加信任列表.
+    NSString * string = @"mqq://im/chat?chat_type=wpa&uin=742308143&version=1&src_type=web";
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:string]]];
+    [self.view addSubview:webView];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];

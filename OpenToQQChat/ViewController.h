fdcad32eb20f53186e643be9712a660343d073ce
苹果 <pingguo@pingguodemacbook-pro.local>
2016-05-25 11:50:13 +0800

    Initial Commit

---
+++ OpenToQQChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenToQQChat
+//
+//  Created by 苹果 on 16/5/25.
+//  Copyright © 2016年 苹果. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OpenToQQChat/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OpenToQQChat
+//
+//  Created by 苹果 on 16/5/25.
+//  Copyright © 2016年 苹果. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
